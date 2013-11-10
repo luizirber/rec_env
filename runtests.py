994e0846df484d4e9b6244db0de11f7fb7047641
Luiz Irber <luiz.irber@gmail.com>
2013-11-10 19:23:01 -0200

    Fix runtests.py

--- runtests.py
+++ runtests.py
@@ -2,4 +2,5 @@
 
 import pytest
 
-pytest.main("--cov rec_env --cov-report xml --cov-report term-missing --junitxml=tests.xml")
+errcode = pytest.main("--cov rec_env --cov-report xml --cov-report term-missing --junitxml=tests.xml")
+raise SystemExit(errcode)
