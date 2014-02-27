204cea90f61890111a26a060e4c7e54dda97e0b3
requires.io <support@requires.io>
2014-02-27 12:37:35 -0800

    [requires.io] dependency update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.md')).read()
 NEWS = open(os.path.join(here, 'NEWS.txt')).read()
 
-version = "0.0.2"
+version = "0.0.3"
 
 install_requires = [
     'PyYAML'
